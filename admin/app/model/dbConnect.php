a458e3ed86a16b748feb1ba9814284bdcc92bd32
Olger Castro <olger60019castro@gmail.com>
2022-03-29 12:17:42 -0500

    rl 29032022

--- admin/app/model/dbConnect.php
+++ admin/app/model/dbConnect.php
@@ -4,9 +4,9 @@ class Conexion
     static public function conectar()
     {
         $link = new PDO(
-            "mysql:host=localhost;dbname=sysclaims",
-            "root",
-            ''
+            "mysql:host=localhost;dbname=db-paus-web",
+            "pausweb-admin",
+            '!Wu96Sl&uCVm^2cMwg@6'
         );
         $link->exec("set names utf8");
         return $link;
--- app/models/dbConnect.php
+++ app/models/dbConnect.php
@@ -1,9 +1,9 @@
 <?php
 class Conexion{
 	static public function conectar(){
-		$link = new PDO("mysql:host=localhost;dbname=sysclaims",
-			            "root",
-			            "");
+		$link = new PDO("mysql:host=localhost;dbname=db-paus-web",
+			            "pausweb-admin",
+			            "!Wu96Sl&uCVm^2cMwg@6");
 		$link->exec("set names utf8");
 		return $link;
 	}
