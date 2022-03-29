8ddc79da8161d89af37f21b4d8747d6a3044d70b
Olger Castro <olger60019castro@gmail.com>
2022-03-29 11:17:09 -0500

    release oficial 29032022

--- gdcsystem/home/controllers/sexoController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class ControladorSexo{
-    static public function ctrListarSexo($item,$valor){
-        $tabla = "lr_tab_sexo_usuario";
-        $respuesta = ModeloSexo::mdlListarSexo($tabla, $item, $valor);
-        return $respuesta;
-    }
-}
